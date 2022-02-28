038b5f4ddfc4821547d82227c28821fb42057cf3
PetarDjukic1 <petardjukic23@gmail.com>
2022-02-28 22:49:02 +0100

    navbar commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-function toggleMobileMenu(menu)
-{
-    menu.classList.toggle('open');
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+ 
+//Toggle Nav
+    burger.addEventListener('click', () => {
+ 
+        nav.classList.toggle('nav-active');
+        //burger animation
+burger.classList.toggle('burger-active');
+    });
+ 
+ 
 }
+ 
+navSlide();
